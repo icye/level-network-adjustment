5f96bc41ba3ab761fd04f0c8147e2a1a8fdcf2ca
icye <951394723@qq.com>
2017-12-17 20:07:05 +0800

    first code

--- level-network-adjustment/Program.cs
+++ level-network-adjustment/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 
 namespace level_network_adjustment
 {
@@ -10,6 +13,7 @@ namespace level_network_adjustment
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
